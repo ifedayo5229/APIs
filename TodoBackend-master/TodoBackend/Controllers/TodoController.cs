de460d885aa8bfd8ba78df3d1bb571850b5725e6
ifedayo5229 <62813326+ifedayo5229@users.noreply.github.com>
2020-12-29 05:51:07 -0800

    Add files via upload

---
+++ TodoBackend-master/TodoBackend/Controllers/TodoController.cs
@@ -0,0 +1,77 @@
+﻿using System;
+using System.Linq;
+using Microsoft.AspNetCore.Mvc;
+using TodoBackend.Models;
+using TodoBackend.ViewModels;
+
+namespace TodoBackend.Controllers
+{
+    [Route ("api/[controller]")]
+    [ApiController]
+    public class TodoController : ControllerBase
+    {
+        private readonly ITodoRepository _repo;
+
+        public TodoController(ITodoRepository repo)
+        {
+            _repo = repo;
+        }
+
+        [Route("getalltodo")]
+        [HttpGet]
+        public IActionResult Home()
+        {
+            var allTodoItems = _repo.GetAll();
+            var views = allTodoItems
+                .Select(r => new TodoItemView(r, Request));
+            return Ok(views);
+        }
+
+        [Route("posttodo")]
+        [HttpPost]
+        public IActionResult NewTodo(TodoItem item)
+        {
+            item.Id = Guid.NewGuid().ToString();
+            item.Completed = false;
+
+            _repo.AddNew(item);
+
+            return Ok(new TodoItemView(item, Request));
+        }
+
+        [Route("gettodo/{id}")]
+        [HttpGet]
+        public IActionResult GetTodo(string id)
+        {
+            var item = _repo.Get(id);
+            return Ok(new TodoItemView(item, Request));
+        }
+
+        [Route("deletetodo/{id?}")]
+        [HttpDelete]
+        public IActionResult DeleteTodo(string id)
+        {
+            if (!string.IsNullOrEmpty(id))
+            {
+                _repo.Delete(id);
+                return Ok();
+            }
+
+            _repo.DeleteAll();
+            return Ok();
+        }
+
+        [Route("edittodo/{id}")]
+        [HttpPatch]
+        public IActionResult ChangeTodo(string id, TodoItem todo)
+        {
+            todo.Id = id;
+
+            _repo.Update(todo);
+
+            var item = _repo.Get(todo.Id);
+
+            return Ok(new TodoItemView(item, Request));
+        }
+    }
+}
